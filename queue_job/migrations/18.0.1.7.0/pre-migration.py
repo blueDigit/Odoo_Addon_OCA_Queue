212e54a4fd2647f2e9d515eacd72294b1c01f14a
Oliver Rojas <oliver.rojas@bluedigit.ca>
2026-02-16 21:17:13 -0300

    Make queue_job migrations odoo.sh compatible

--- queue_job/migrations/18.0.1.7.0/pre-migration.py
+++ queue_job/migrations/18.0.1.7.0/pre-migration.py
@@ -1,11 +1,11 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+from odoo import api, SUPERUSER_ID
 from openupgradelib import openupgrade
 
 
-@openupgrade.migrate()
-def migrate(env, version):
+def migrate(cr, version):
     # Remove cron garbage collector
     openupgrade.delete_records_safely_by_xml_id(
-        env,
+        api.Environment(cr, SUPERUSER_ID, {}),
         ["queue_job.ir_cron_queue_job_garbage_collector"],
     )
